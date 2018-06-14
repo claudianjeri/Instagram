de23d59b7f9978015d19e5aae63addc4131a64a5
claudianjeri <claudianjeri04@gmail.com>
2018-06-14 12:57:44 +0300

    added various classes and templates

--- GrAm/models.py
+++ GrAm/models.py
@@ -2,4 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Images(models.Model): # inherits from the modules.Model clss
-    image = 
+    image = models.ImageField(upload_to = 'uploads/')
+    caption = models.TextField()
+    comments = models.TextField()
+    profile = 
+    likes =
+    upload_date=
+
+
+class Profile(models.Model):
+
+
+class User(models.Model):
+
+
+class Comments(models.Model):
+
+class Likes(models.Model):
+
+
+
