7e1fd91d65bb53c6b9f472252d660bc63c84ecb7
claudianjeri <claudianjeri04@gmail.com>
2018-06-17 13:41:38 +0300

    registered the models

--- GrAm/admin.py
+++ GrAm/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Image, Comment, Profile
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Profile)
--- GrAm/models.py
+++ GrAm/models.py
@@ -1,6 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    profilePic = models.ImageField(upload_to='profile/',null=True,blank=True)
+    bio = models.CharField(max_length=60,blank=True)
+    # user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile(cls):
+        profile = Profile.objects.all()
+        return profile
+
+    @classmethod
+    def find_profile(cls,search_term):
+        profile = cls.objects.filter(user__username__icontains=search_term)
+        return profile
+
+    @classmethod
+    def update_profile(cls,id,bio):
+        updated = Image.objects.filter(id=id).update(bio = bio)
+        return updated 
+
+
+
 class Image(models.Model):
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     caption = models.CharField(max_length = 60)
@@ -34,38 +66,6 @@ class Image(models.Model):
     def get_image_by_id(cls,id):
         image = Image.objects.filter(id=Image.id)
         return image        
-
-class Profile(models.Model):
-    profilePic = models.ImageField(upload_to='profile/',null=True,blank=True)
-    bio = models.CharField(max_length=60,blank=True)
-    # user = models.ForeignKey(User,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.bio
-
-    def save_profile(self):
-        self.save()
-
-    def delete_profile(self):
-        self.delete()
-
-    @classmethod
-    def get_profile(cls):
-        profile = Profile.objects.all()
-        return profile
-
-    @classmethod
-    def find_profile(cls,search_term):
-        profile = cls.objects.filter(user__username__icontains=search_term)
-        return profile
-
-    @classmethod
-    def update_profile(cls,id,bio):
-        updated = Image.objects.filter(id=id).update(bio = bio)
-        return updated 
-
-
-
 
 class Comment(models.Model):
     comments = models.CharField(max_length=60,blank=True,null=True)
