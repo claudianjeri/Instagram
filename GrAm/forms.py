a27bdba822f4b22a9f4bf1dfcf76560aac94c85f
claudianjeri <claudianjeri04@gmail.com>
2018-06-18 06:55:52 +0300

    added uploadform class

--- GrAm/forms.py
+++ GrAm/forms.py
@@ -4,3 +4,8 @@ class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+class UploadForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['user','likes','upload_date','profile']
--- GrAm/views.py
+++ GrAm/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Image, Profile, Comment
-from .forms import EditProfileForm
+from .forms import EditProfileForm, UploadForm
 # Create your views here.
 def start(request):
 
